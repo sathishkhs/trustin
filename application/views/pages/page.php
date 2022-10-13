86f32692747830177ebda129a385a83f735e2a9c
sathish kumar <67854806+sathishds@users.noreply.github.com>
2022-10-13 20:11:02 +0530

    4th

--- application/views/pages/page.php
+++ application/views/pages/page.php
@@ -1,166 +1,182 @@
-<section class="hero-about-area" style="background-image: url(<?php echo (!empty($page_items->banner_image)) ? (BANNER_IMAGE_PATH.$page_items->banner_image) : 'front/images/inner-banner.jpg'; ?>);">
-			<div class="container">
-				<div class="row">
-					<div class="col-lg-12">
-						<div class="hero-about-text text-left">
-							<h2><?php echo $page_items->page_title; ?></h2>
-							<h4><span><a href="">Home</a> - </span><?php echo $page_items->page_title; ?></h4>
-						</div>
-					</div>
+<section class="hero-about-area" style="background-image: url(<?php echo (!empty($page_items->banner_image)) ? (BANNER_IMAGE_PATH . $page_items->banner_image) : 'front/images/inner-banner.jpg'; ?>);">
+	<div class="container">
+		<div class="row">
+			<div class="col-lg-12">
+				<div class="hero-about-text text-left">
+					<h2><?php echo $page_items->page_title; ?></h2>
+					<h4><span><a href="">Home</a> - </span><?php echo $page_items->page_title; ?></h4>
 				</div>
 			</div>
-		</section>
+		</div>
+	</div>
+</section>
 
-		
-			<section class="common-text-area" style="background:#fcfcfc;">
-		    <div class="sec-title text-center">
-							<h2>Trust-in Hospital Current Events List</h2>
+
+<div class="container mt-5">
+	<div class="row">
+		<div class="col-sm-12 tabs-wrapper">
+
+			<div class="tv-tabs">
+				<label class="tv-tab present-tab" for="tv-tab-1">Active Events</label>
+				<label class="tv-tab past-tab" for="tv-tab-2">Past Events</label>
+
+			</div>
+			<input class="tv-radio" id="tv-tab-1" name="tv-group" type="radio" checked="checked" />
+			<div class="tv-content present">
+				<h4 style="text-align:center"> No Active Events Currently</h4>
+			</div>
+			<input class="tv-radio" id="tv-tab-2" name="tv-group" type="radio" />
+			<div class="tv-content">
+				<div class="container">
+					<div class="row" id="list-doctors">
+
+						<div class="team-block-two col-lg-4 col-md-6 col-sm-12">
+							<div class="inner-box">
+								<div class="image-box">
+									<figure class="image"><a href="independence_day"><img src="uploads/events/independence_day-mobile-event.jpg" alt=""></a></figure>
+									<!--<ul class="social-links">-->
+									<!--	<li><a target="_blank" href="<?php echo $doctor->facebook_link; ?>"><span class="fab fa-facebook-f"></span></a></li>-->
+									<!--	<li><a target="_blank" href="<?php echo $doctor->twitter_link; ?>"><span class="fab fa-twitter"></span></a></li>-->
+									<!--	<li><a target="_blank" href="<?php echo $doctor->instagram_link; ?>"><span class="fab fa-instagram"></span></a></li>-->
+									<!--	<li><a target="_blank" href="<?php echo $doctor->linkedin_link; ?>"><span class="fab fa-linkedin-in"></span></a></li>-->
+									<!--</ul>-->
+								</div>
+								<div class="info-box">
+									<h5 class="name"><a href="independence_day">Freedom Offer August 2022</a></h5>
+									<span class="designation"><b>Held on:</b> 15<sup>th</sup> – 31<sup>st</sup> August 2022</span>
+									<span class="designation">Freedom from Cancer</span>
+								</div>
+							</div>
 						</div>
-			<div class="container">
-				<div class="row" id="list-doctors">
-                   <div class="col-sm-12">
-                   <h4 style="text-align:center">	No Active Events Currently</h4>
-                   </div>
-                 
-                   
-				
-					
-					
-					
-				
+
+
+						<div class="team-block-two col-lg-4 col-md-6 col-sm-12">
+							<div class="inner-box">
+								<div class="image-box">
+									<figure class="image"><a href="trustin-run"><img src="uploads/events/trustin-run-card.jpg" alt=""></a></figure>
+									<!--<ul class="social-links">-->
+									<!--	<li><a target="_blank" href="<?php echo $doctor->facebook_link; ?>"><span class="fab fa-facebook-f"></span></a></li>-->
+									<!--	<li><a target="_blank" href="<?php echo $doctor->twitter_link; ?>"><span class="fab fa-twitter"></span></a></li>-->
+									<!--	<li><a target="_blank" href="<?php echo $doctor->instagram_link; ?>"><span class="fab fa-instagram"></span></a></li>-->
+									<!--	<li><a target="_blank" href="<?php echo $doctor->linkedin_link; ?>"><span class="fab fa-linkedin-in"></span></a></li>-->
+									<!--</ul>-->
+								</div>
+								<div class="info-box">
+									<h5 class="name"><a href="trustin-run">Trust-in Run November 2021</a></h5>
+									<span class="designation"><b>Held on:</b> 14<sup>th</sup> November 2021, Sunday</span>
+									<span class="designation">To spread awareness about Breast Cancer</span>
+								</div>
+							</div>
+						</div>
+
+						<div class="team-block-two col-lg-4 col-md-6 col-sm-12">
+							<div class="inner-box">
+								<div class="image-box">
+									<figure class="image"><a href="blood_donation_camp"><img src="uploads/events/blood-donation-camp.jpg" alt=""></a></figure>
+									<!--<ul class="social-links">-->
+									<!--	<li><a target="_blank" href="<?php echo $doctor->facebook_link; ?>"><span class="fab fa-facebook-f"></span></a></li>-->
+									<!--	<li><a target="_blank" href="<?php echo $doctor->twitter_link; ?>"><span class="fab fa-twitter"></span></a></li>-->
+									<!--	<li><a target="_blank" href="<?php echo $doctor->instagram_link; ?>"><span class="fab fa-instagram"></span></a></li>-->
+									<!--	<li><a target="_blank" href="<?php echo $doctor->linkedin_link; ?>"><span class="fab fa-linkedin-in"></span></a></li>-->
+									<!--</ul>-->
+								</div>
+								<div class="info-box">
+									<h5 class="name"><a href="blood_donation_camp">Blood Donation Camp 2022</a></h5>
+									<span class="designation"><b>Held on:</b> 08<sup>th</sup> January 2022</span>
+									<span class="designation">A single blood donation can save up to 3 lives!</span>
+								</div>
+							</div>
+						</div>
+
+						<div class="team-block-two col-lg-4 col-md-6 col-sm-12">
+							<div class="inner-box">
+								<div class="image-box">
+									<figure class="image"><a href="arthritis"><img src="uploads/events/arthrities-camp.jpg" alt=""></a></figure>
+									<!--<ul class="social-links">-->
+									<!--	<li><a target="_blank" href="<?php echo $doctor->facebook_link; ?>"><span class="fab fa-facebook-f"></span></a></li>-->
+									<!--	<li><a target="_blank" href="<?php echo $doctor->twitter_link; ?>"><span class="fab fa-twitter"></span></a></li>-->
+									<!--	<li><a target="_blank" href="<?php echo $doctor->instagram_link; ?>"><span class="fab fa-instagram"></span></a></li>-->
+									<!--	<li><a target="_blank" href="<?php echo $doctor->linkedin_link; ?>"><span class="fab fa-linkedin-in"></span></a></li>-->
+									<!--</ul>-->
+								</div>
+								<div class="info-box">
+									<h5 class="name"><a href="arthritis">Arthritis 2021</a></h5>
+									<span class="designation"><b>Held on:</b> 11<sup>th</sup> & 12<sup>th</sup> December 2021</span>
+									<span class="designation">Arthritis is one of the topmost causes for disability.</span>
+								</div>
+							</div>
+						</div>
+
+
+						<div class="team-block-two col-lg-4 col-md-6 col-sm-12">
+							<div class="inner-box">
+								<div class="image-box">
+									<figure class="image"><a href="women_health_camp"><img src="uploads/events/women_health_event.jpg" alt=""></a></figure>
+									<!--<ul class="social-links">-->
+									<!--	<li><a target="_blank" href="<?php echo $doctor->facebook_link; ?>"><span class="fab fa-facebook-f"></span></a></li>-->
+									<!--	<li><a target="_blank" href="<?php echo $doctor->twitter_link; ?>"><span class="fab fa-twitter"></span></a></li>-->
+									<!--	<li><a target="_blank" href="<?php echo $doctor->instagram_link; ?>"><span class="fab fa-instagram"></span></a></li>-->
+									<!--	<li><a target="_blank" href="<?php echo $doctor->linkedin_link; ?>"><span class="fab fa-linkedin-in"></span></a></li>-->
+									<!--</ul>-->
+								</div>
+								<div class="info-box">
+									<h5 class="name"><a href="women_health_camp">Women Health Camp March 2022</a></h5>
+									<span class="designation"><b>Held on: </b>8<sup>th</sup> – 13<sup>th</sup> March 2022</span>
+									<span class="designation">Women’s health is important & it needs to be handled sensitively. </span>
+								</div>
+							</div>
+						</div>
+
+						<div class="team-block-two col-lg-4 col-md-6 col-sm-12">
+							<div class="inner-box">
+								<div class="image-box">
+									<figure class="image"><a href="surgical_camp"><img src="uploads/events/surgical_event.jpg" alt=""></a></figure>
+									<!--<ul class="social-links">-->
+									<!--	<li><a target="_blank" href="<?php echo $doctor->facebook_link; ?>"><span class="fab fa-facebook-f"></span></a></li>-->
+									<!--	<li><a target="_blank" href="<?php echo $doctor->twitter_link; ?>"><span class="fab fa-twitter"></span></a></li>-->
+									<!--	<li><a target="_blank" href="<?php echo $doctor->instagram_link; ?>"><span class="fab fa-instagram"></span></a></li>-->
+									<!--	<li><a target="_blank" href="<?php echo $doctor->linkedin_link; ?>"><span class="fab fa-linkedin-in"></span></a></li>-->
+									<!--</ul>-->
+								</div>
+								<div class="info-box">
+									<h5 class="name"><a href="surgical_camp">General Surgery Camp March 2022</a></h5>
+									<span class="designation"><b>Held on: </b>10<sup>th</sup> March – 10<sup>th</sup> April 2022</span>
+									<span class="designation">Experience, precision & aftercare are critical in all surgeries. </span>
+								</div>
+							</div>
+						</div>
+
+
+
+
+					</div>
 				</div>
 			</div>
-		</section>
-		
-		
-		<!-- common content -->
-		<section class="common-text-area" style="background:#fcfcfc;">
-		    <div class="sec-title text-center">
-							<h2>Trust-in Hospital Completed Events List</h2>
-						</div>
-			<div class="container">
-				<div class="row" id="list-doctors">
-				    
-				    <div class="team-block-two col-lg-4 col-md-6 col-sm-12">
-						<div class="inner-box">
-							<div class="image-box">
-							   <figure class="image"><a href="independence_day"><img src="uploads/events/independence_day-mobile-event.jpg" alt=""></a></figure>
-								<!--<ul class="social-links">-->
-								<!--	<li><a target="_blank" href="<?php echo $doctor->facebook_link ; ?>"><span class="fab fa-facebook-f"></span></a></li>-->
-								<!--	<li><a target="_blank" href="<?php echo $doctor->twitter_link ; ?>"><span class="fab fa-twitter"></span></a></li>-->
-								<!--	<li><a target="_blank" href="<?php echo $doctor->instagram_link ; ?>"><span class="fab fa-instagram"></span></a></li>-->
-								<!--	<li><a target="_blank" href="<?php echo $doctor->linkedin_link ; ?>"><span class="fab fa-linkedin-in"></span></a></li>-->
-								<!--</ul>-->
-							</div>
-							<div class="info-box">
-								<h5 class="name"><a href="independence_day">Freedom Offer August 2022</a></h5>
-								<span class="designation"><b>Held on:</b> 15<sup>th</sup> – 31<sup>st</sup> August 2022</span>
-								<span class="designation">Freedom from Cancer</span>
-							</div>
-						</div>
-					</div>
-					
-               
-					<div class="team-block-two col-lg-4 col-md-6 col-sm-12">
-						<div class="inner-box">
-							<div class="image-box">
-							   <figure class="image"><a href="trustin-run"><img src="uploads/events/trustin-run-card.jpg" alt=""></a></figure>
-								<!--<ul class="social-links">-->
-								<!--	<li><a target="_blank" href="<?php echo $doctor->facebook_link ; ?>"><span class="fab fa-facebook-f"></span></a></li>-->
-								<!--	<li><a target="_blank" href="<?php echo $doctor->twitter_link ; ?>"><span class="fab fa-twitter"></span></a></li>-->
-								<!--	<li><a target="_blank" href="<?php echo $doctor->instagram_link ; ?>"><span class="fab fa-instagram"></span></a></li>-->
-								<!--	<li><a target="_blank" href="<?php echo $doctor->linkedin_link ; ?>"><span class="fab fa-linkedin-in"></span></a></li>-->
-								<!--</ul>-->
-							</div>
-							<div class="info-box">
-								<h5 class="name"><a href="trustin-run">Trust-in Run November 2021</a></h5>
-								<span class="designation"><b>Held on:</b> 14<sup>th</sup> November 2021, Sunday</span>
-								<span class="designation">To spread awareness about Breast Cancer</span>
-							</div>
-						</div>
-					</div>
-					
-						<div class="team-block-two col-lg-4 col-md-6 col-sm-12">
-						<div class="inner-box">
-							<div class="image-box">
-							   <figure class="image"><a href="blood_donation_camp"><img src="uploads/events/blood-donation-camp.jpg" alt=""></a></figure>
-								<!--<ul class="social-links">-->
-								<!--	<li><a target="_blank" href="<?php echo $doctor->facebook_link ; ?>"><span class="fab fa-facebook-f"></span></a></li>-->
-								<!--	<li><a target="_blank" href="<?php echo $doctor->twitter_link ; ?>"><span class="fab fa-twitter"></span></a></li>-->
-								<!--	<li><a target="_blank" href="<?php echo $doctor->instagram_link ; ?>"><span class="fab fa-instagram"></span></a></li>-->
-								<!--	<li><a target="_blank" href="<?php echo $doctor->linkedin_link ; ?>"><span class="fab fa-linkedin-in"></span></a></li>-->
-								<!--</ul>-->
-							</div>
-							<div class="info-box">
-								<h5 class="name"><a href="blood_donation_camp">Blood Donation Camp 2022</a></h5>
-								<span class="designation"><b>Held on:</b> 08<sup>th</sup> January 2022</span>
-								<span class="designation">A single blood donation can save up to 3 lives!</span>
-							</div>
-						</div>
-					</div>
-					
-						<div class="team-block-two col-lg-4 col-md-6 col-sm-12">
-						<div class="inner-box">
-							<div class="image-box">
-							   <figure class="image"><a href="arthritis"><img src="uploads/events/arthrities-camp.jpg" alt=""></a></figure>
-								<!--<ul class="social-links">-->
-								<!--	<li><a target="_blank" href="<?php echo $doctor->facebook_link ; ?>"><span class="fab fa-facebook-f"></span></a></li>-->
-								<!--	<li><a target="_blank" href="<?php echo $doctor->twitter_link ; ?>"><span class="fab fa-twitter"></span></a></li>-->
-								<!--	<li><a target="_blank" href="<?php echo $doctor->instagram_link ; ?>"><span class="fab fa-instagram"></span></a></li>-->
-								<!--	<li><a target="_blank" href="<?php echo $doctor->linkedin_link ; ?>"><span class="fab fa-linkedin-in"></span></a></li>-->
-								<!--</ul>-->
-							</div>
-							<div class="info-box">
-								<h5 class="name"><a href="arthritis">Arthritis 2021</a></h5>
-								<span class="designation"><b>Held on:</b> 11<sup>th</sup> & 12<sup>th</sup> December 2021</span>
-								<span class="designation">Arthritis is one of the topmost causes for disability.</span>
-							</div>
-						</div>
-					</div>
-					
-					
-					<div class="team-block-two col-lg-4 col-md-6 col-sm-12">
-						<div class="inner-box">
-							<div class="image-box">
-							   <figure class="image"><a href="women_health_camp"><img src="uploads/events/women_health_event.jpg" alt=""></a></figure>
-								<!--<ul class="social-links">-->
-								<!--	<li><a target="_blank" href="<?php echo $doctor->facebook_link ; ?>"><span class="fab fa-facebook-f"></span></a></li>-->
-								<!--	<li><a target="_blank" href="<?php echo $doctor->twitter_link ; ?>"><span class="fab fa-twitter"></span></a></li>-->
-								<!--	<li><a target="_blank" href="<?php echo $doctor->instagram_link ; ?>"><span class="fab fa-instagram"></span></a></li>-->
-								<!--	<li><a target="_blank" href="<?php echo $doctor->linkedin_link ; ?>"><span class="fab fa-linkedin-in"></span></a></li>-->
-								<!--</ul>-->
-							</div>
-							<div class="info-box">
-								<h5 class="name"><a href="women_health_camp">Women Health Camp March 2022</a></h5>
-								<span class="designation"><b>Held on: </b>8<sup>th</sup> – 13<sup>th</sup> March 2022</span>
-								<span class="designation">Women’s health is important & it needs to be handled sensitively. </span>
-							</div>
-						</div>
-					</div>
-					
-						<div class="team-block-two col-lg-4 col-md-6 col-sm-12">
-						<div class="inner-box">
-							<div class="image-box">
-							   <figure class="image"><a href="surgical_camp"><img src="uploads/events/surgical_event.jpg" alt=""></a></figure>
-								<!--<ul class="social-links">-->
-								<!--	<li><a target="_blank" href="<?php echo $doctor->facebook_link ; ?>"><span class="fab fa-facebook-f"></span></a></li>-->
-								<!--	<li><a target="_blank" href="<?php echo $doctor->twitter_link ; ?>"><span class="fab fa-twitter"></span></a></li>-->
-								<!--	<li><a target="_blank" href="<?php echo $doctor->instagram_link ; ?>"><span class="fab fa-instagram"></span></a></li>-->
-								<!--	<li><a target="_blank" href="<?php echo $doctor->linkedin_link ; ?>"><span class="fab fa-linkedin-in"></span></a></li>-->
-								<!--</ul>-->
-							</div>
-							<div class="info-box">
-								<h5 class="name"><a href="surgical_camp">General Surgery Camp March 2022</a></h5>
-								<span class="designation"><b>Held on: </b>10<sup>th</sup> March – 10<sup>th</sup> April 2022</span>
-								<span class="designation">Experience, precision & aftercare are critical in all surgeries. </span>
-							</div>
-						</div>
-					</div>
-					
-					
-					
-				
-				</div>
-			</div>
-		</section>
-		
+
+
+
+
+
+
+
+		</div>
+	</div>
+</div>
+
+
+<script>
+	$('.tv-tab').click(function(){
+		$('.tv-tab').removeClass('active');
+		$(this).addClass('active');
+	})
+
+	$(document).ready(function(){
+		if($('.present').find('.team-block-two').length !== 0){
+			$('.present-tab').click();
+			$('.present-tab').addClass('active')
+		}else{
+			$('.past-tab').click();
+			$('.past-tab').addClass('active');
+		}
+	})
+</script>
